4eceac652cb62a454de8813f9c2df56f836f3fcf
Azamat Gilmanshin <ttapent@gmail.com>
2024-11-15 03:01:39 +0500

    Добавьте файлы проекта.

---
+++ OrderWindow.xaml.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Baiguzin41size
+{
+    /// <summary>
+    /// Логика взаимодействия для OrderWindow.xaml
+    /// </summary>
+    public partial class OrderWindow : Window
+    {
+        public event Action OrderWindowClosed;
+        List<OrderProduct> selectedOrderProducts = new List<OrderProduct>();
+        List<Product> selectedProducts = new List<Product>();
+        private Order currentOrder = new Order();
+        //private OrderProduct orderOrderProduct = new OrderProduct();
+        User currentUser;
+        private double Cost = 0;
+        private int SetDeliveryDay(List<Product> products)
+        {
+
+            bool DeliveryStatus = false;
+            foreach (var p in products)
+            {
+                if (p.inStock <= 3)
+                {
+                    DeliveryStatus = true;
+                }
+            }
+
+            if (DeliveryStatus)
+                return 6;
+            else
+                return 3;
+        
+    }
+
+
+
+        public OrderWindow(List<OrderProduct> selectedOrderProducts, List<Product> selectedProducts, User user)
+        {
+            InitializeComponent();
+
+            currentUser = user;
+            Cost = 0;
+            var currentPickups = Gilmanshin_41Entities.GetContext().PickupPoint.ToList();
+            PickupsCombo.ItemsSource = currentPickups.Select(x => $"{x.PickupPointIndex}, {x.PickupPointCity}, {x.PickupPointStreet}, {x.PickupPointHouseNumber}"); 
+            PickupsCombo.SelectedIndex = 0;
+            int currentID = selectedOrderProducts.First().OrderID; 
+            currentOrder.OrderID = currentID;
+
+            List<Order> allOrderCodes = Gilmanshin_41Entities.GetContext().Order.ToList();
+            List<int> OrderCodes = new List<int>();
+            foreach (var p in allOrderCodes.Select(x => $"{x.OrderCode}").ToList())
+            {
+                OrderCodes.Add(Convert.ToInt32(p));
+            }
+            Random random = new Random();
+
+            while (true)
+            {
+                int num = random.Next(100, 1000);
+                if (!OrderCodes.Contains(num))
+                {
+                    currentOrder.OrderCode = num;
+                    break;
+                }
+            }
+
+
+            foreach (Product p in selectedProducts)
+            {
+                var matchingOrderProduct = selectedOrderProducts.FirstOrDefault(q => q.ProductArticleNumber == p.ProductArticleNumber);
+
+                if (matchingOrderProduct != null)
+                {
+                    p.Quantityto = (int)matchingOrderProduct.OrderQuantity;
+
+                    if (p.ProductQuantityInStock >= p.Quantityto)
+                    {
+                        p.ProductQuantityInStock -= p.Quantityto;
+                    }
+                    else
+                    {
+                        p.Quantityto = p.ProductQuantityInStock;
+                        p.ProductQuantityInStock = 0;
+                    }
+                }
+                else
+                {
+                    p.Quantityto = 1;
+                }
+            }
+
+
+
+            this.selectedOrderProducts = selectedOrderProducts;
+            this.selectedProducts = selectedProducts;
+
+
+            for (int i = 0; i < selectedProducts.Count; i++)
+            {
+                Cost += (Convert.ToDouble(selectedProducts[i].ProductCost) - Convert.ToDouble(selectedProducts[i].ProductCost) * Convert.ToDouble(selectedProducts[i].ProductDiscountAmount) / 100) * selectedProducts[i].Quantityto;
+            }
+
+            TotalCost.Text = Cost.ToString();
+
+            OrderDP.Text = DateTime.Now.ToString();
+            OrderDD.Text = DateTime.Now.AddDays(SetDeliveryDay(selectedProducts)).ToString();
+
+            TBOrderID.Text = currentID.ToString();
+            ProductListView.ItemsSource = selectedProducts;
+
+
+            if (currentUser != null)
+            {
+                currentOrder.OrderUserId = user.UserID;
+                FIOTB.Text = user.UserSurname + " " + user.UserName + " " + user.UserPatronymic;
+            }
+            else
+            {
+                FIOTB.Text = "Гость";
+                currentOrder.OrderUserId = null;
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            currentOrder.OrderPickupPoint = PickupsCombo.SelectedIndex + 1;
+            currentOrder.OrderDate = DateTime.Now;
+            currentOrder.OrderDeliveryDate = DateTime.Now.AddDays(SetDeliveryDay(selectedProducts));
+            currentOrder.OrderStatus = "Новый";
+            currentOrder.OrderCode = currentOrder.OrderCode;
+            
+            foreach (var p in selectedOrderProducts)
+            {
+                Gilmanshin_41Entities.GetContext().OrderProduct.Add(p);
+            }
+
+            Gilmanshin_41Entities.GetContext().Order.Add(currentOrder);
+
+            try
+            {
+                Gilmanshin_41Entities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                selectedOrderProducts.Clear();
+                selectedProducts.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+            this.Close();
+        }
+
+        private void BtnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            Cost = 0;
+            var prod = (sender as Button).DataContext as Product;
+            var selectedOP = selectedOrderProducts.FirstOrDefault(p => p.ProductArticleNumber == prod.ProductArticleNumber);
+            int index = selectedOrderProducts.IndexOf(selectedOP);
+            selectedOrderProducts[index].Amout++;
+            if (prod.ProductQuantityInStock > 0)
+            {
+                prod.Quantityto++;
+                prod.ProductQuantityInStock--;
+            }
+
+
+            ProductListView.Items.Refresh();
+            for (int i = 0; i < selectedProducts.Count; i++)
+            {
+                Cost += (Convert.ToDouble(selectedProducts[i].ProductCost) - Convert.ToDouble(selectedProducts[i].ProductCost) * Convert.ToDouble(selectedProducts[i].ProductDiscountAmount) / 100) * selectedProducts[i].Quantityto;
+            }
+
+            TotalCost.Text = Cost.ToString();
+            OrderDD.Text = DateTime.Now.AddDays(SetDeliveryDay(selectedProducts)).ToString();
+
+
+        }
+
+        private void BtnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            var prod = (sender as Button).DataContext as Product;
+            prod.Quantityto--;
+            Cost = 0;
+            var selectedOP = selectedProducts.FirstOrDefault(p => p.ProductArticleNumber == prod.ProductArticleNumber);
+            int index = selectedProducts.IndexOf(selectedOP);
+
+            if (prod.Quantityto == 0)
+            {
+                ProductPage.btnActivity = false;
+                selectedOrderProducts[index].Amout = 0;
+                var pr = ProductListView.SelectedItem as Product;
+                selectedOrderProducts.RemoveAt(index);
+                selectedProducts.RemoveAt(index);
+                if (ProductListView.Items.Count == 0)
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                selectedOrderProducts[index].Amout--;
+                    prod.ProductQuantityInStock++;
+            }
+            for (int i = 0; i < selectedProducts.Count; i++)
+            {
+                Cost += (Convert.ToDouble(selectedProducts[i].ProductCost) - Convert.ToDouble(selectedProducts[i].ProductCost) * Convert.ToDouble(selectedProducts[i].ProductDiscountAmount) / 100) * selectedProducts[i].Quantityto;
+            }
+            OrderDD.Text = DateTime.Now.AddDays(SetDeliveryDay(selectedProducts)).ToString();
+            TotalCost.Text = Cost.ToString();
+            ProductListView.Items.Refresh();
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var prod = (sender as Button).DataContext as Product;
+            prod.Quantityto = 0;
+            var selectedOP = selectedProducts.FirstOrDefault(p => p.ProductArticleNumber == prod.ProductArticleNumber);
+            int index = selectedProducts.IndexOf(selectedOP);
+            selectedOrderProducts[index].Amout = 0;
+            var pr = ProductListView.SelectedItem as Product;
+            selectedOrderProducts.RemoveAt(index);
+            selectedProducts.RemoveAt(index);
+
+            for (int i = 0; i < selectedProducts.Count; i++)
+            {
+                Cost += (Convert.ToDouble(selectedProducts[i].ProductCost) - Convert.ToDouble(selectedProducts[i].ProductCost) * Convert.ToDouble(selectedProducts[i].ProductDiscountAmount) / 100) * selectedProducts[i].Quantityto;
+            }
+            TotalCost.Text = Cost.ToString();
+            ProductListView.Items.Refresh();
+            OrderDD.Text = DateTime.Now.AddDays(SetDeliveryDay(selectedProducts)).ToString();
+            if (ProductListView.Items.Count == 0)
+            {
+                this.Close();
+            }
+        }
+    }
+}
---
+++ ProductPage.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Baiguzin41size
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        int countfull = 0;
+        int count = 0;
+        User currentUser;
+        int newOrderId;
+        public static bool btnActivity = false;
+        int Quantity = 0;
+
+        List<Product> selectedProducts = new List<Product>();
+        List<OrderProduct> selectedOrderProduct = new List<OrderProduct>();
+       
+        public ProductPage(User user)
+        {
+            InitializeComponent();
+
+            if (selectedProducts.Count == 0)
+            {
+                BTNOrder.Visibility = Visibility.Hidden;
+            }
+            currentUser = user;
+
+            if (user != null)
+            {
+
+                FIOTB.Text = user.UserSurname + "" + user.UserName + "" + user.UserPatronymic;
+                switch (user.UserRole)
+                {
+                    case 1:
+                        RoleTB.Text = "Клиент"; break;
+                    case 2:
+                        RoleTB.Text = "Менеджер"; break;
+                    case 3:
+                        RoleTB.Text = "Администратор"; break;
+                }
+            }
+            else
+            {
+                FIOTB.Text = "Гость";
+                RoleTB.Text = "";
+            }
+
+            ComboType2.SelectedIndex = 0;
+            
+            var currentProduct = Gilmanshin_41Entities.GetContext().Product.ToList();
+            
+            ProductListView.ItemsSource = currentProduct;
+            
+            countfull = Gilmanshin_41Entities.GetContext().Product.Count();
+           
+
+            UpdateProduct();
+
+        }
+
+        private void Go_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPAge());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+
+        private void RBtnUP_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void RbtnDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void UpdateProduct()
+        {
+            var currentProduct = Gilmanshin_41Entities.GetContext().Product.ToList();
+            if (ComboType2.SelectedIndex ==0)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount <= 100)).ToList();
+            }
+            if (ComboType2.SelectedIndex == 1)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount < 10)).ToList();
+            }
+            if (ComboType2.SelectedIndex ==  2)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount >= 10 && p.ProductDiscountAmount < 15)).ToList();
+            }
+            if (ComboType2.SelectedIndex == 3)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount >= 15 && p.ProductDiscountAmount < 100)).ToList();
+            }
+
+
+            currentProduct = currentProduct.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            if (RBtnUP.IsChecked.Value)
+            {
+                currentProduct = currentProduct.OrderBy(p => p.ProductCost).ToList();
+            }
+            if (RbtnDown.IsChecked.Value)
+            {
+                currentProduct = currentProduct.OrderByDescending(p => p.ProductCost).ToList();
+            }
+
+            ProductListView.ItemsSource = currentProduct;
+            count = currentProduct.Count();
+            CountTB.Text = $"Выведено {count} из {countfull}";
+        }
+
+        private void ComboType2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProductListView.SelectedIndex >= 0)
+            {
+                var prod = ProductListView.SelectedItem as Product;
+                var existingOrderProduct = selectedOrderProduct.FirstOrDefault(p => p.ProductArticleNumber == prod.ProductArticleNumber);
+
+                if (existingOrderProduct != null)
+                {
+                    existingOrderProduct.OrderQuantity++;
+
+                    var selectedProduct = selectedProducts.FirstOrDefault(p => p.ProductArticleNumber == prod.ProductArticleNumber);
+                    if (selectedProduct != null)
+                    {
+                        selectedProduct.Quantityto = existingOrderProduct.OrderQuantity;
+                    }
+                }
+                else
+                {
+                    var newOrderProd = new OrderProduct
+                    {
+                        OrderID = newOrderId,
+                        ProductArticleNumber = prod.ProductArticleNumber,
+                        OrderQuantity = 1
+                    };
+                    selectedOrderProduct.Add(newOrderProd);
+                    prod.Quantityto = 1; 
+                    selectedProducts.Add(prod);
+                }
+
+                BTNOrder.Visibility = Visibility.Visible;
+
+                ProductListView.Items.Refresh();
+
+                ProductListView.SelectedIndex = -1;
+            }
+            UpdateProduct();
+        }
+
+        private void ShoeListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void BTNOrder_Click(object sender, RoutedEventArgs e)
+        {
+            selectedProducts = selectedProducts.Distinct().ToList();
+            
+            OrderWindow orderWindows = new OrderWindow(selectedOrderProduct, selectedProducts, currentUser  );
+            orderWindows.ShowDialog();
+            BTNOrder.Visibility = Visibility.Hidden;
+            selectedOrderProduct.Clear();
+        }
+    }
+}
